6d5fc86c8e18fc93577e2dd5066e3f6377bcedd7
austin <business.mocksock@gmail.com>
2022-05-31 14:46:42 -0500

    Fix UserData to User

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { tap, catchError } from 'rxjs/operators';
-import { UserData } from "./user-data.service";
+import { User } from "./user.data";
 
 @Injectable({
     providedIn: 'root'
@@ -27,8 +27,8 @@ export class DataService {
         return throwError(error);
     }
 
-    getUsers(): Observable<UserData[]> {
-        return this.http.get<UserData[]>(this.apiurl).pipe(
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(this.apiurl).pipe(
             tap(data => console.log(data)),
             catchError(this.handleError)
         );
