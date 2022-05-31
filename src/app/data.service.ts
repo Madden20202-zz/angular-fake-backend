33b6742f3af8b91248784799689e567a58d6a05b
austin <business.mocksock@gmail.com>
2022-05-31 05:57:33 -0500

    add data service class

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,3 +4,25 @@ import { Observable, throwError } from "rxjs";
 import { tap, catchError } from 'rxjs/operators';
 import { User } from "./user.data";
 
+@Injectable({
+    providedIn: 'root'
+})
+
+export class DataService {
+    
+    apiurl = 'api/users';
+    headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
+    httpOptions = {
+        headers: this.headers
+    };
+
+    // injectable service to communicate 
+    // with the data 
+    constructor(private http: HttpClient) {}
+
+    // function will handle and log errors
+    private handleError(error: any) { 
+        console.error(error);
+        return throwError(error);
+    }
+}
