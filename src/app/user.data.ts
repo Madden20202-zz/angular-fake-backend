0d1410787aaffdeea02450cc69a82532e8f4956c
austin <business.mocksock@gmail.com>
2022-06-03 08:42:36 -0500

    reformat User class

--- src/app/user.data.ts
+++ src/app/user.data.ts
@@ -1,3 +1,8 @@
 export class User {
-    constructor( public id: number, public name: string, public email: string, public contact: string) {}
+    constructor( 
+        public id: number, 
+        public name: string, 
+        public email: string, 
+        public contact: string
+    ) {}
 }
